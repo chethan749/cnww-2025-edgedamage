51d02cdbba7a81c46f2d817288a29ae46906dcc3
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2025-12-19 12:02:43 -0800

    pytorch prototype working

--- optim_edgedamage_pytorch.py
+++ optim_edgedamage_pytorch.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 import numpy as np
 import networkx as nx
+import itertools
+import copy
 
 def markov_metrics(P):
     '''
@@ -53,13 +55,13 @@ def markov_metrics(P):
     return tau_global,kc_i[0]
 
 class damn_edge(nn.Module):
-    def __init__(self, adj):
+    def __init__(self, adj, ne):
         super().__init__()
-        
         
         self.flatten = nn.Flatten() # needed?
         self.A0 = torch.Tensor(adj)
         self.n = self.A0.shape[0]
+        self.ne = ne
         
         # make optimizeable via nn.Parameter
         self.B = nn.Parameter(torch.zeros((self.n,self.n)), requires_grad=True)
@@ -67,21 +69,14 @@ class damn_edge(nn.Module):
         self.P0 = torch.reshape((1/torch.sum(self.A0, axis=1)),(self.n,1)) * self.A0
         return
 
-    def forward(self, x):
-        '''
-        in: an adjacency matrix of modifications (can/should be negative for 
-        edge deletions)
-        
-        out: directed travel time.
-        '''
-        
+    def tau_global(self,x):
         I = torch.eye(self.n)
         #e = np.ones(n, dtype=int)
         e = torch.ones(self.n)
         
         _A = torch.Tensor(self.A0 + x)
         _P = torch.reshape((1/torch.sum(_A, axis=1)),(self.n,1)) * _A
-        ev,lv = torch.linalg.eig(_P) # eig values and left eig vectors
+        ev,lv = torch.linalg.eig(_P.T) # eig values and left eig vectors
         ev = torch.real(ev)
         lv = torch.real(lv)
         
@@ -109,28 +104,90 @@ class damn_edge(nn.Module):
         tau_i = torch.sum(tau_mat,axis=0) # vector of tau values
         
         tau_global = torch.sum(tau_i) / self.n
+        return tau_global
+    #
+    def forward(self, x, record=False):
+        '''
+        in: an adjacency matrix of modifications (can/should be negative for 
+        edge deletions)
         
-        # total mass penalty.
-        _l = torch.abs(torch.abs(torch.sum(x.flatten())) - 2)
+        out: directed travel time.
+        '''
         
-        #return tau_global + _l
-        return _l
+        _tau = self.tau_global(x)
+        _B_vec1norm = self.adjnorm(x)
+        
+        _l = torch.abs(_B_vec1norm - self.ne)
+
+        return _tau + self.mu*_l # todo: arbitrary multiplier
     #
     
-    def train(self, nit=1000, lr=1e-6):
+    def adjnorm(self,x):
+        return torch.linalg.norm(x.flatten(),1)
+    #
+    
+    def train(self, nit=1000, lr=1e-3, print_every=100, record_every=100, record_jumps=False, mu=1e-1):
         optimizer = torch.optim.SGD(model.parameters(), lr=lr)
         
-        for k in range(nit):
-            tau = self(self.B)
-            tau.backward()
-            optimizer.step() # descend
+        self.mu=mu
+        
+        self.steps = [0]
+        print(torch.Tensor(self.B.detach()))
+        
+        import copy
+        
+        self.snapshots = [copy.deepcopy(torch.Tensor(self.B.detach()))]
+        self.taus = [self.tau_global(self.B)]
+        self.Bnorms = [self.adjnorm(self.B)]
+        self._prev = self(self.B) # initial
+        self.objectives = [self.forward(self.B)]
+        
+        for k in range(1,nit):
+            try:
+                obj = self(self.B)
+                obj.backward()
+                optimizer.step() # descend
+                
+                optimizer.zero_grad()
+            except:
+                # expect the linalg_eig_backward error happened, record and exit.
+                print('OH NO', k, abs(self._prev - obj)/abs(self._prev))
+                self.steps.append(k)
+                self.snapshots.append( copy.deepcopy(torch.Tensor(self.B.detach())) )
+                self.taus.append( self.tau_global(self.B) )
+                self.Bnorms.append( self.adjnorm(self.B) )
+                self.objectives.append( self(self.B) )
+                print('Eig error, halting.')
+                break
+            # save checkpoints, print, etc.
+            if k%record_every==0:
+                self.steps.append(k)
+                self.snapshots.append( copy.deepcopy(torch.Tensor(self.B.detach())) )
+                self.taus.append( self.tau_global(self.B) )
+                self.Bnorms.append( self.adjnorm(self.B) )
+                self.objectives.append( self(self.B) )
+                self._prev = self(self.B) # initial
+                
+                #import pdb
+                #pdb.set_trace()
+            elif record_jumps and abs(self._prev - obj)/abs(self._prev) > 0.5:
+                print('OH NO', k, abs(self._prev - obj)/abs(self._prev))
+                self.steps.append(k)
+                self.snapshots.append( torch.Tensor(self.B).detach() )
+                self.taus.append( self.tau_global(self.B) )
+                self.Bnorms.append( self.adjnorm(self.B) )
+                self.objectives.append( self(self.B) )
+                self._prev = self(self.B) # initial
+            #
             
-            
-            optimizer.zero_grad()
-            if k%100==0:
-                print(f'{k} : {tau:.2f}')
+            if k%print_every==0:
+                print(f'iter: {k:7} : tau: {self.tau_global(self.B):3.2f} -- norm(B): {self.adjnorm(self.B):4.1f}')
+            # update
+            self._prev = obj
+        #
+        
         return self.B
-
+    #
 
 if __name__=="__main__":
     from matplotlib import pyplot as plt
@@ -138,9 +195,87 @@ if __name__=="__main__":
 
     plt.style.use('tableau-colorblind10')
     plt.rcParams.update({'font.size': 14})
-
-    G = tools.directed_rectangle(2,3)
+    
+    m,n = 2,2
+    G = tools.directed_rectangle(2,2)
+    target_num_edges = 2
+    
     A = nx.to_numpy_array(G)
-    model = damn_edge(A)
-    model.train(1000, lr=1e-1)
+    
+    # random initialization
+    torch.manual_seed(0)
+    #B0 = torch.rand(A.shape)
+    B0 = torch.zeros(A.shape)
+    #B0 = -target_num_edges*B0/torch.linalg.norm(B0.flatten(),1)
+    
+    model = damn_edge(A, target_num_edges)
+    #model.B = nn.Parameter(B0, requires_grad=True)
+    nepoch = 10000
+    model.train(nepoch, 
+        mu=1,
+        lr=1e-4, 
+        record_every=nepoch//100, 
+        print_every=nepoch//100, 
+        record_jumps=True
+    )
+    
+    ptrs={idx:i for i,idx in enumerate(model.steps)}
+    
+    # visualize
+    fig,ax = plt.subplot_mosaic('''
+    TTTAD
+    TTTBE
+    TTTCF
+    NNN..
+    ''',
+    figsize=(10,6),
+    constrained_layout=True)
+    
+    bn=torch.Tensor(model.Bnorms).detach().numpy()
+    objective=torch.Tensor(model.objectives).detach().numpy()
+    taus=torch.Tensor(model.taus).detach().numpy()
+    
+    ax['T'].plot(model.steps, taus, label=r'$\tau(\vec{G})$')
+    #ax['T'].set_yscale('log')
+    
+    ax['N'].plot(model.steps, bn, label=r'$||B||_\mathrm{vec}$')
+    
+    kmax = len(model.steps)
+    
+    for idx,l in zip(np.arange(0,kmax,kmax//6)[:6],'ABCDEF'):
+        #A_damaged = (model.A0 + model.snapshots[idx]).detach().numpy()
+        
+        # threshold perturb.
+        perturb = (model.snapshots[idx]).detach().numpy()
+        perturb[abs(perturb)<1e-2]=0
+        
+        pos = {i+n*j:(i,j) for i,j in itertools.product(range(m),range(n))}
+        
+        #if idx==0:
+        #    to_vis = model.A0
+        #else:
+        #    to_vis = perturb
+        #
+        to_vis = model.A0 + perturb
+        
+        tools.plot_weighted_graph(
+            to_vis, ax=ax[l], 
+            min_width=0.0, max_width=2, pos=pos,
+            with_edge_labels=True
+        )
+        
+        ax[l].text(0,1, l, ha='left', va='top', bbox={'facecolor':'#ffd'})
+        ax['T'].scatter(model.steps[idx],taus[idx], marker='*', c='#333', s=200)
+        ax['T'].text(model.steps[idx],taus[idx], l, fontsize=16, ha='left', va='bottom', bbox={'facecolor':'#ffd'}, zorder=-100)
+    #
+    
+    ax['T'].set(xticklabels=[], ylabel=r'$\tau$')
+    ax['T'].legend()
+    ax['N'].set(xlabel='epoch', ylabel='matrix_norm')
+    ax['N'].legend()
+    ax['N'].axhline(target_num_edges, c='#333', lw=0.5, ls='--')
+    
+    fig.show()
+    OUT_FOLDER = 'outputs/'
+    fig.savefig(OUT_FOLDER+'edge_damage_optimization_out.png', dpi=300, bbox_inches='tight')
     
--- tools.py
+++ tools.py
@@ -7,7 +7,7 @@
 import numpy as np
 from scipy.linalg import eig
 import networkx as nx
-
+import torch
 
 
 def markov_metrics(P):
@@ -96,18 +96,28 @@ class network_passage_analyzer:
             raise Exception('Could not fetch eigenvectors/values; run preprocess() first')
         
         n = self.n # size of square nxn matrix
-        I = np.identity(n, dtype=int)
-        e = np.ones(n, dtype=int)
+        #I = np.identity(n, dtype=int)
+        I = torch.eye(n)
+        #e = np.ones(n, dtype=int)
+        e = torch.ones(n)
 
         # calculate the stationary vector pi
         #ev,lv = eig(P,left=True, right=False) # eig values and left eig vectors
+        ev,lv = torch.linalg.eig(torch.Tensor(self.P.T))
+        # cast to real...?
+        ev = torch.real(ev)
+        lv = torch.real(lv)
         
-        ix = np.argsort(ev)[-1] # dominant eigvalue index
-        dom_eigenvector = lv[:,ix].real
-        pi = (1/np.sum(dom_eigenvector))*dom_eigenvector # probabiltiy vector
+        #ix = np.argsort(ev)[-1] # dominant eigvalue index
+        ix = torch.argsort(ev)[-1]
+        #dom_eigenvector = lv[:,ix].real
+        dom_eigenvector = lv[:,ix]
+        pi = (1/torch.sum(dom_eigenvector))*dom_eigenvector # probabiltiy vector
+        self.steady_real_resid = max(abs(dom_eigenvector - lv[:,ix]))
         
         # calculate the fundamental matrix Z
-        W = np.outer(e,pi) # pi in every row
+        #W = np.outer(e,pi) # pi in every row
+        W = torch.outer(e,pi)
         #try:
         #    Z = np.linalg.inv(I - P + W)
         #except:
@@ -115,37 +125,52 @@ class network_passage_analyzer:
         #    import pdb
         #    pdb.set_trace()
         
-        Z = np.linalg.inv(I - self.P + W)
+        #Z = np.linalg.inv(I - self.P + W)
+        Z = torch.linalg.inv(I - torch.Tensor(self.P) + W)
 
         # construct the mean first passage matrix M
-        E = np.ones((n,n), dtype=int)
-        Z_diag = np.diag(np.diag(Z))
-        D = np.diag(1/pi)
-        mat1 = (I - Z + np.matmul(E,Z_diag))
-        M = np.matmul(mat1,D)
-        np.fill_diagonal(M, 0) # set M[i][i]=0
+        #E = np.ones((n,n), dtype=int)
+        E = torch.ones((n,n))
+        #Z_diag = np.diag(np.diag(Z))
+        Z_diag = torch.diag(torch.diag(Z))
+        #D = np.diag(1/pi)
+        D = torch.diag(1/pi)
+        #mat1 = (I - Z + np.matmul(E,Z_diag))
+        mat1 = (I - Z + E@Z_diag)
+        #M = np.matmul(mat1,D)
+        M = mat1@D
+        #np.fill_diagonal(M, 0) # set M[i][i]=0
+        M = M - torch.diag(torch.diag(M))
 
         self.M = M
         
         # calculate the global time tau
-        pi_mat = np.outer(pi,e) # pi in every column
+        pi_mat = torch.outer(pi,e) # pi in every column
         tau_mat = pi_mat * M 
-        tau_i = np.sum(tau_mat,axis=0) # vector of tau values
-        tau_global = np.sum(tau_i) / n
+        tau_i = torch.sum(tau_mat,axis=0) # vector of tau values
+        tau_global = torch.sum(tau_i) / n
         #print('tau: ' + str(tau_global))
         
         # calculate kemeny's constant
-        kc_mat = W * M 
-        kc_i = np.sum(kc_mat,axis=1) # vector of kc values (should all be the same)
+        #if False:
+        #kc_mat = W * M #entrywise product
+        #kc_i = torch.sum(kc_mat,axis=1) # vector of kc values (should all be the same)
         
-        self.kemeny = kc_i[0]
+        #self.kemeny = kc_i[0]
         self.tau_mat = tau_mat
         self.tau_i = tau_i
         self.tau_global = tau_global
         
-        return tau_global,kc_i[0]
+        return tau_global.item()
 
 #
+
+#
+###############
+#
+
+
+
 def cycle_and_chain(cycle_length=3, chain_length=0, undirected=False):
     '''
     inputs: cycle_length, chain_length, integers
@@ -184,4 +209,158 @@ def directed_rectangle(m,n, undirected=False):
     return G
 #
 
+def plot_weighted_graph(adj_matrix,
+                        ax=None,
+                        node_labels=None,
+                        pos=None,
+                        figsize=(8, 6),
+                        min_width=0.5,
+                        max_width=8.0,
+                        with_labels=True,
+                        with_edge_labels=False,
+                        edge_label_format="{:.2g}"):
+    """
+    Credit: ChatGPT slop code.
+    
+    Create a NetworkX plot where edge thickness is proportional to edge weight.
 
+    Parameters
+    ----------
+    adj_matrix : np.ndarray
+        Square adjacency matrix. Zero entries are treated as no-edge.
+    ax : matplotlib.axes.Axes or None
+        Axis to draw on. If None, a new figure and axis are created.
+    node_labels : dict or None
+        Optional mapping {node_index: label} to use for node labels.
+    pos : dict or None
+        Optional node positions as {node: (x,y)}. If None, a spring layout is used.
+    figsize : tuple
+        Figure size used only if ax is None.
+    min_width, max_width : float
+        Minimum and maximum line width for edges.
+    with_labels : bool
+        Whether to draw node labels.
+    with_edge_labels : bool
+        Whether to draw edge labels (defaults to False).
+    edge_label_format : str
+        Format string used to convert edge weights to labels.
+        
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        Axis containing the drawn graph.
+    """
+    # Validate adjacency matrix
+    adj = np.array(adj_matrix, dtype=float)
+    if adj.ndim != 2 or adj.shape[0] != adj.shape[1]:
+        raise ValueError("adj_matrix must be a square 2D numpy array")
+
+    # Create axis if needed
+    if ax is None:
+        _, ax = plt.subplots(figsize=figsize)
+
+    # Choose directed vs undirected based on symmetry
+    directed = not np.allclose(adj, adj.T)
+    G = nx.from_numpy_array(adj, create_using=nx.DiGraph() if directed else nx.Graph())
+
+    # Remove zero-weight edges defensively
+    edges_to_remove = [(u, v) for u, v, d in G.edges(data=True)
+                       if d.get("weight", 0) == 0]
+    if edges_to_remove:
+        G.remove_edges_from(edges_to_remove)
+
+    # Prepare positions
+    if pos is None:
+        pos = nx.spring_layout(G)
+
+    # Handle empty-edge case (nodes only)
+    if G.number_of_edges() == 0:
+        nx.draw_networkx_nodes(G, pos=pos, ax=ax)
+        if with_labels:
+            labels = node_labels if node_labels else {n: n for n in G.nodes()}
+            nx.draw_networkx_labels(G, pos=pos, labels=labels, ax=ax)
+        ax.set_axis_off()
+        return ax
+
+    # Extract and normalize weights
+    edge_data = list(G.edges(data=True))
+    weights = np.array([d.get("weight", 1.0) for _, _, d in edge_data], dtype=float)
+
+    w_min, w_max = weights.min(), weights.max()
+    if w_max > w_min:
+        widths = min_width + (weights - w_min) / (w_max - w_min) * (max_width - min_width)
+    else:
+        widths = np.full_like(weights, (min_width + max_width) / 2.0)
+
+    # Draw nodes
+    nx.draw_networkx_nodes(G, pos=pos, ax=ax)
+
+    # Draw edges
+    edgelist = [(u, v) for u, v, _ in edge_data]
+    if directed:
+        nx.draw_networkx_edges(
+            G, pos=pos, ax=ax,
+            edgelist=edgelist,
+            width=widths,
+            arrows=True,
+            arrowstyle='-|>',
+            arrowsize=12
+        )
+    else:
+        nx.draw_networkx_edges(
+            G, pos=pos, ax=ax,
+            edgelist=edgelist,
+            width=widths
+        )
+
+    # Draw labels
+    if with_labels:
+        labels = node_labels if node_labels else {n: n for n in G.nodes()}
+        nx.draw_networkx_labels(G, pos=pos, labels=labels, ax=ax)
+    
+    if with_edge_labels:
+        edge_labels = {
+            (u, v): edge_label_format.format(d.get("weight", 1.0))
+            for u, v, d in edge_data
+        }
+        nx.draw_networkx_edge_labels(
+            G,
+            pos=pos,
+            edge_labels=edge_labels,
+            ax=ax
+        )
+    
+    ax.set_axis_off()
+    return ax
+#
+
+
+if __name__=='__main__':
+    def comparison(inputs):
+        # directed calculation
+        G = directed_rectangle(inputs[0], inputs[1])
+        #A = nx.to_numpy_array(G)
+        #P = (1/np.sum(A,axis=1)) * A # row-sums one.
+        po_d = network_passage_analyzer(G)
+        po_d.preprocess()
+        tau_d = po_d.markov_metrics()
+        
+        # undirected
+        G_U = nx.to_undirected(G)
+        po_u = network_passage_analyzer(G_U)
+        po_u.preprocess()
+        tau_u = po_u.markov_metrics()
+        return tau_u,tau_d
+    #
+    R = np.zeros((8,8))
+    for i in range(8):
+        for j in range(8):
+            tau_u, tau_d = comparison((i+2,j+2))
+            R[i,j] = tau_u/tau_d
+    #
+    from matplotlib import pyplot as plt
+    
+    fig,ax = plt.subplots()
+    ax.matshow(R.T, cmap=plt.cm.PRGn, vmin=0., vmax=2)
+    fig.show()
+    
